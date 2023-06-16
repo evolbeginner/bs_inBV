d05cf09235ef852c32305c7a43a46e8899f7af92
Evolbeginner <sishuowang@hotmail.ca>
2023-06-16 09:42:44 +0800

    Add files via upload

--- from_bs_to_hessian.R
+++ from_bs_to_hessian.R
@@ -19,17 +19,8 @@ m <- as.matrix(read.table(infile))
 
 # this is to ensure that those columns where all have exactly the same value will not as they will cause the covariance matrix of
 # that line all equal to zero, an irreversible matrix.
-for(col in 1:ncol(m)){
-	unique_count <- length(unique(m[,col]))
-	#if(unique_count <= 50){
-	if(unique_count == 1 & m[1,col] <= 1e-3){
-	#if(unique_count <= 2){
-		print(col)
-		m[,col] <- m[,col] * rnorm(nrow(m), 1, 8e-3)
-		#print(m[m[,col]<0, col])
-		#m[,col] <- rexp(length(m[,col]), 1)
-	}
-}
+
+m <- m + diag(ncol(m))*1e-6
 
 c <- cov(m)
 

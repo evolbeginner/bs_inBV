68f7bd7384e5ff7200b6afedb43fd22bf657b421
Evolbeginner <sishuowang@hotmail.ca>
2023-06-16 09:43:47 +0800

    Update from_bs_to_hessian.R

--- from_bs_to_hessian.R
+++ from_bs_to_hessian.R
@@ -15,11 +15,6 @@ outfile = args[2]
 ############################################
 m <- as.matrix(read.table(infile))
 
-#m <- m * rnorm(ncol(m), 1, 8e-3)
-
-# this is to ensure that those columns where all have exactly the same value will not as they will cause the covariance matrix of
-# that line all equal to zero, an irreversible matrix.
-
 m <- m + diag(ncol(m))*1e-6
 
 c <- cov(m)
